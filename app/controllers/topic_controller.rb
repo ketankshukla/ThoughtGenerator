b648f7b8cab5680489a5fc82dd3cb224e52be63a
Ketan Shukla <ketankshukla@gmail.com>
2015-10-22 04:46:09 -0700

    Added topics controller and show view for the home controller

---
+++ app/controllers/topic_controller.rb
@@ -0,0 +1,8 @@
+class TopicController < ApplicationController
+
+  def show
+    #show a sample topic in the page when a button is pressed
+    rand_id = rand(Topic.count)
+    @topic = Topic.first(:conditions => [ "id >= ?", rand_id])
+  end
+end
