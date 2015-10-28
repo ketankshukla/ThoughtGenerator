b943a57b0a95640e969bac06ab99a195ce9514c2
Ketan Shukla <ketankshukla@gmail.com>
2015-10-28 15:36:07 -0700

    Removed unnecessary files

--- app/controllers/topics_controller.rb
+++
@@ -1,9 +0,0 @@
-class TopicsController < ApplicationController
-
-  def show
-    #show a sample topic in the page when a button is pressed
-    #rand_id = rand(Topic.count)
-    #@topic = Topic.first(:conditions => [ "id >= ?", rand_id])
-  end
-end
-
